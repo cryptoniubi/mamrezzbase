43d6e0e21488367b53845f0d2a1314cd124dd9a1
Mohammadreza Mohammadi <92263217+cryptoniubi@users.noreply.github.com>
2023-07-20 13:06:10 +0200

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x7c99ddd1ff6259855a7aed109825b9d50a8a7a5a";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
